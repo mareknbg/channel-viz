88b4a0edffde2cea730d84ee586575b86b095259
mareknbg <mareknbg@googlemail.com>
2014-06-03 14:34:25 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= '5WBy8ZAkxtRXFvzXsKtROKVRdU9iH6RwgLbXXi9tiOvwgy4N', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [708309428,259675843], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Markus Test', // Replaces Xively logo in the header
+		applicationName	= 'IoT Markus', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
