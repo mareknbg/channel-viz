6b26407bab388b2d1d8d17b562a24a383ba1b8b9
mareknbg <mareknbg@googlemail.com>
2014-07-16 11:48:25 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -150,7 +150,7 @@
 									var graph = new Rickshaw.Graph( {
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 600,
-										height: 900,
+										height: 300,
 										renderer: 'area',
 									        interpolation: 'cardinal',
 									        stroke: true,
